f84249c7fd141c36aca4e11d2370eef47de1749a
RohanUD <78956997+RohanUD@users.noreply.github.com>
2022-10-31 21:37:49 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("hands");
+var y = document.getElementById("animcon");
+function closeye() {
+  x.style.marginTop = "0%";
+}
+function openeye() {
+  x.style.marginTop = "90%";
+}
